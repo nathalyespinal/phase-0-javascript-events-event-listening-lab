7bb7b85ecac97bd5e8d8f74acc722162bf1b8585
NathalyEspinal <espinalnathaly@gmail.com>
2022-01-25 18:23:18 -0600

    Completed assignment

--- index.js
+++ index.js
@@ -1,2 +1,13 @@
+let input = document.getElementById('input');
+
 function addingEventListener() {
+     
+  
+     input.addEventListener('click', function(){
+      alert ('I was clicked!')
+
+     } );    
+
 }
+
+
